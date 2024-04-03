f0b208093c349c0ccf3c5374b60da18a6c727137
SamirKhan21860 <msamirkhan561@gmail.com>
2024-04-03 18:30:25 +0500

    So Thankfully the :hover with a closing curly bracket were placed on the right spot

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -9,15 +9,15 @@ const Header = (props) => {
         </Logo>
         <NavMenu>
           <a href="/home">
-            <img src="/images/home-icon.svg" alt="Home" />
+            <img src="/images/home-icon.svg" alt="home" />
             <span>Home</span>
           </a>
           <a href="/search">
-            <img src="/images/search-icon.svg" alt="Search" />
+            <img src="/images/search-icon.svg" alt="search" />
             <span>Search</span>
           </a>
-          <a href="/Watchlist">
-            <img src="/images/watchlist-icon.svg" alt="Watchlist" />
+          <a href="/watchlist">
+            <img src="/images/watchlist-icon.svg" alt="watchlist" />
             <span>Watchlist</span>
           </a>
           <a href="/originals">
@@ -60,6 +60,7 @@ const Logo = styled.a`
   max-height: 70px;
   font-size: 0;
   display: inline-block;
+
   img {
     display: block;
     width: 100%;
@@ -71,58 +72,60 @@ const NavMenu = styled.div`
   display: flex;
   flex-flow: row nowrap;
   height: 100%;
-  justify-content: end;
+  justify-content: flex-end;
+  margin: 0px;
+  padding: 0px;
   position: relative;
-  margin: 0 auto 0 25px;
+  margin-right: auto;
+  margin-left: 25px;
 
   a {
     display: flex;
     align-items: center;
     padding: 0 12px;
-  }
 
-  img {
-    height: 20px;
-    min-width: 20px;
-    width: 20px;
-    z-index: auto;
-  }
+    img {
+      height: 20px;
+      min-width: 20px;
+      width: 20px;
+      z-index: auto;
+    }
 
-  span {
-    color: rgb(249, 249, 249);
-    font-size: 13px;
-    letter-spacing: 1.42px;
-    line-height: 1.08;
-    padding: 2px 0px;
-    white-space: nowrap;
-    position: relative;
+    span {
+      color: rgb(249, 249, 249);
+      font-size: 13px;
+      letter-spacing: 1.42px;
+      line-height: 1.08;
+      padding: 2px 0px;
+      white-space: nowrap;
+      position: relative;
 
-    &:before {
-      background-color: rgb(249, 249, 249);
-      border-radius: 0px 0px 4px 4px;
-      bottom: -4px;
-      content: "";
-      height: 2px;
-      opacity: 0;
-      position: absolute;
-      left: 0px;
-      right: 0px;
-      transform-origin: left center;
-      transform: scaleX(0);
-      transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
-      visibility: hidden;
-      width: auto;
+      &:before {
+        background-color: rgb(249, 249, 249);
+        border-radius: 0px 0px 4px 4px;
+        bottom: -6px;
+        content: "";
+        height: 2px;
+        opacity: 0;
+        position: absolute;
+        left: 0px;
+        right: 0px;
+        transform-origin: left center;
+        transform: scaleX(0);
+        transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+        visibility: hidden;
+        width: auto;
+      }
+    }
+
+    &:hover {
+      span:before {
+        transform: scaleX(1);
+        visibility: visible;
+        opacity: 1 !important;
+      }
     }
   }
-
-  &:hover {
-    span:before {
-      transform: scaleX(1);
-      visibility: visible;
-      opacity: 1 !important;
-    }
-  }
-}
 
   // @media (max-width: 768px) {
   //   display: none;
